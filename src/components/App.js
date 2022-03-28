bc9537eca629c79a0ff93073bba4627fbeb9b1a9
kritigya <97790296+kritigya@users.noreply.github.com>
2022-03-28 22:58:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component } from 'react'
 
-const App = () => {
-  return (
-    <div id="main"></div>
-  )
+export class App extends Component {
+  render() {
+    return (
+      <div id="root">
+        <p>I am learning React. My life is getting better.</p>
+      </div>
+    )
+  }
 }
 
-
-export default App;
+export default App
